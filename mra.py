bba5fc591dedb80a08fe372f03d742bfdf095203
Cold Moon <deluna.lin@gmail.com>
2012-09-15 15:56:47 +0800

    MRA redone

--- mra.py
+++ mra.py
@@ -1,3 +1,13 @@
-#This is a comment lol
-import psutil
+#what
+import statgrab
 print("Machine Resource Agent deploying...")
+
+#data are displayed in bytes I think
+#RAM and CPU stats
+print(statgrab.sg_get_cpu_stats())
+
+#HD Space
+print(statgrab.sg_get_mem_stats())
+
+#still need to work on parsing
+
